e27c60a02b1e7dc8e4f18c4ba50e59c47336f7b3
becodewala-youtube <kvikash74673@gmail.com>
2025-06-06 18:01:27 +0530

    z-index fiex for langing hero section

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -25,9 +25,9 @@ const Landing = () => {
       }`}
     >
       {/* Hero Section */}
-      <div className="relative overflow-hidden flex flex-col-reverse lg:block ">
+      <div className="relative overflow-hidden flex flex-col-reverse lg:block  ">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="relative z-10 pb-8 sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+          <div className="relative z-5 pb-8 sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
             <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
               <motion.div
                 className="sm:text-center lg:text-left"
